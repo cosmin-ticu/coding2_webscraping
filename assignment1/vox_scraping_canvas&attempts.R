c8a2764b3dee869a3f581eb69f7bd9d43c072198
Cosmin Catalin Ticu <cosmin.cata99@gmail.com>
2020-11-21 13:01:46 +0200

    finalized assignment 1 - scrape is working

--- assignment1/vox_scraping_canvas&attempts.R
+++ assignment1/vox_scraping_canvas&attempts.R
@@ -1,7 +1,15 @@
 library(rvest)
 library(data.table)
 
-# my_url_vox <- 'https://www.vox.com/search?q=trump'
+my_url_vox <- 'https://www.vox.com/search?q=trump'
+t <- read_html(my_url_vox)
+boxes <- t %>% html_nodes('.c-entry-box--compact')
+tl <- list()
+tl[['author']] <- boxes %>% html_nodes('.c-byline-wrapper') %>% html_nodes('.c-byline__item') %>% html_text()
+gigi <- boxes %>% html_nodes('.c-byline-wrapper')
+
+boxes[7] %>% html_nodes('.c-byline__item') %>% html_nodes('a') %>%  html_text()
+boxes[7] %>% html_nodes('.c-byline__author-name') %>% html_text() %>% toString()
 
 get_one_page_from_vox <- function(my_url_vox) {
   
@@ -14,7 +22,11 @@ get_one_page_from_vox <- function(my_url_vox) {
     tl <- list()
     
     tl[['title']] <- x %>% html_nodes('.c-entry-box--compact__title') %>% html_text()
-    tl[['author']] <- x %>% html_nodes('.c-byline__author-name') %>% html_text()
+    
+    
+    tl[['author']] <- x %>% html_nodes('.c-byline__author-name') %>% html_text() %>% toString()
+    
+    
     tl[['timestamp']] <- x %>% html_nodes('time.c-byline__item') %>% html_attr('datetime') %>% strtrim(10) %>% as.Date()
     tl[['link']] <- x %>% html_nodes('.c-entry-box--compact__image-wrapper') %>% html_attr('href')
     
@@ -34,11 +46,18 @@ get_vox_data <- function(searchterm, pages_to_download) {
   # concatenate the search terms together according to URL
   searchterm <- gsub(' ','+',searchterm)
   # create links
-  links_to_get <- 
+  if (pages_to_download == 1){
+  
+    links_to_get <- paste0('https://www.vox.com/search?q=',searchterm)}
+  
+  else{
+    
     c(paste0('https://www.vox.com/search?order=date&q=',searchterm),paste0('https://www.vox.com/search?order=date&page=',2:pages_to_download,'&q=',searchterm))
+  
+  }
   ret_df <- rbindlist(lapply(links_to_get, get_one_page_from_vox))
   return(ret_df)
   
 }
 
-kukus <- get_vox_data('trump biden',5) # algorithm cannot yet deal with multiple authors; it lists them as having written separate articles
+kukus <- get_vox_data('trump',1) # algorithm cannot yet deal with multiple authors; it lists them as having written separate articles
